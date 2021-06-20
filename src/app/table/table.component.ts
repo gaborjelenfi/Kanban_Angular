723e9254ba629c677d2ba1d6a6c9cbc21a06efb5
Jelenfi Gábor <dzsub4@gmail.com>
2021-06-20 06:42:35 +0200

    added the funcionality to add, edit and delete an item

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,19 +7,38 @@ import {
 } from '@angular/cdk/drag-drop';
 import { NgForm } from '@angular/forms';
 
+interface Task {
+  task: string;
+  priority: string;
+}
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
-  @ViewChild('controlForm', {static: false}) controlForm: NgForm;
-  radio: null;
+  @ViewChild('controlForm', { static: false }) controlForm: NgForm;
+  textarea: string = null;
+  radio: string = null;
+  selectedArray: string = null;
+  selectedElIndex: number = null;
+  isEditMode = false;
+  isDragging = false;
 
-  backLog = [{task:'Task 1', priority:'high'}, {task:'Task 2', priority:'med'}, {task:'Task 3', priority:'low'}];
-  inProgress = [{task:'Task 5', priority:'high'}, {task:'Task 6', priority:'high'}];
-  codeReview = [{task:'Task 7', priority:'low'}];
-  inTest = [{task:'Task 8', priority:'low'}, {task:'Task 9', priority:'med'}, {task:'Task 10', priority:'med'}];
+
+  trash: Task[] = [];
+  backLog: Task[] = [];
+  inProgress: Task[] = [
+    { task: 'Task 5', priority: 'high' },
+    { task: 'Task 6', priority: 'high' },
+  ];
+  codeReview: Task[] = [{ task: 'Task 7', priority: 'low' }];
+  inTest: Task[] = [
+    { task: 'Task 8', priority: 'low' },
+    { task: 'Task 9', priority: 'med' },
+    { task: 'Task 10', priority: 'med' },
+  ];
   done = [];
 
   constructor() {}
@@ -44,11 +63,57 @@ export class TableComponent implements OnInit {
   }
 
   onSubmit() {
+    const task: Task = {
+      task: this.controlForm.value.task,
+      priority: this.controlForm.value.priority,
+    };
+    if (this.isEditMode) {
+      switch (this.selectedArray) {
+        case 'backLog':
+          this.backLog[this.selectedElIndex] = task;
+          break;
+        case 'inProgress':
+          this.inProgress[this.selectedElIndex] = task;
+          break;
+        case 'codeReview':
+          this.codeReview[this.selectedElIndex] = task;
+          break;
+        case 'inTest':
+          this.inTest[this.selectedElIndex] = task;
+          break;
+        case 'done':
+          this.done[this.selectedElIndex] = task;
+          break;
+          default:
+            this.backLog.unshift(task);
+        }
+      } else {
+        this.backLog.unshift(task);
+      }
+      this.onReset();
+      this.isEditMode = false;
+    }
 
+    onReset() {
+      this.radio = null;
+      this.controlForm.resetForm();
+      this.isEditMode = false;
+    }
+
+    onEdit(nameOfArray: string, item: Task, index: number) {
+      this.isEditMode = true;
+      this.selectedArray = nameOfArray;
+      this.textarea = item.task;
+      this.radio = item.priority;
+      this.selectedElIndex = index;
   }
 
-  onReset() {
-    this.radio = null;
-    this.controlForm.resetForm();
+  dragging() {
+    this.isDragging = true;
+  }
+
+  released() {
+    this.isDragging = false;
+    this.trash = [];
   }
 }
